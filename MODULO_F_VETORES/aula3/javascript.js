5264a38d711bad0061d97e6936cf0f2db2e8c9b5
raphaelpalhano <raphael1angel@hotmail.com>
2021-03-13 18:23:01 -0300

    último exercício JAVASCRIPT

--- MODULO_F_VETORES/aula3/javascript.js
+++ MODULO_F_VETORES/aula3/javascript.js
@@ -16,9 +16,6 @@ Um button para finalizar e gerar as mensagens.
 */
 
 // função para somar:
-
-
-
 function sum(vetor){
    let valores = vetor;
    return valores.reduce(function(a, b){
@@ -26,16 +23,19 @@ function sum(vetor){
    })
 }
 
+//function valor mínimo:
 function min(vetor){
     let minimo = Math.min.apply(null, vetor);
     return minimo
 }
 
+//function valor max:
 function max(vetor){
     let maximo = Math.max.apply(null, vetor);
     return maximo
 }
 
+//function: value mean:
 function mean(vetor){
     let media = sum(vetor) / vetor.length
     return media
@@ -45,7 +45,7 @@ let numtxt = document.getElementById('numtxt')
 let number = Number(numtxt.value)
 let table = document.getElementById('tab')
 let list = []
-let listp = []
+
 
 function limited_number(num){
     if (Number(num) >= 1 && Number(num) <= 100){
@@ -65,31 +65,47 @@ function listed(num, list){
 }
 
 
-
-
 function adicionar(){
     if (limited_number(numtxt.value) && !listed(numtxt.value, list))
         {
         list.push(Number(numtxt.value));  
-        listp.push(list[0])
-        c = 0
-        let item = document.createElement('option');
-        item.text = `Valor ${list} adicionado`;
+        let item = document.createElement('option')
+        item.text = `Valor ${numtxt.value} adicionado`;
         table.appendChild(item)
-        list = []
-        if (listp.length > 3){
-            alert(listp)
-        } 
         
+                
+                 
     }
-    
-    
 
     else{
         alert(`[ERROR] Digite valores maiores que zero e que não sejam repetidos!`)
     }
-
+    numtxt.value = '';
+    numtxt.focus();
 
 }
 
 
+function relatorio(){
+    if (list.length == 0){
+        alert('[ERROR] Você não pode gerar o relatório sem nenhum valor na lista!')
+    }
+    else{
+        let writte = document.getElementById('relatorio');
+        writte.innerHTML = ''
+        writte.innerHTML += `O total de elementos na lista é ${list.length} <br>`;
+        writte.innerHTML += `A soma dos elementos na lista é ${sum(list)} <br>`;
+        writte.innerHTML += `O maior valor na lista é ${max(list)} <br>`;
+        writte.innerHTML += `O menor valor na lista é ${min(list)} <br>`;
+        writte.innerHTML += `A média dos valores na lista é ${mean(list)} <br>`;
+    }
+
+    
+
+
+}
+
+function resetar(){
+    table.removeChild(table.childNodes[0])
+    list.length -= 1;
+}
